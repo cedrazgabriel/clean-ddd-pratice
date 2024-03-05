756d7570eef6e4e6101ea9b122a01dc86ff75c60
Gabriel Cedraz <cedrazdev@gmail.com>
2024-03-05 19:00:26 -0300

    feat: Add test:watch script and update AnswerQuestionUseCase to use an answer repository

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import {expect, test} from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { IAnswersRepository } from '../repositories/interfaces/answer-repository'
+import { Answer } from '../entities/answer'
 
-test('Create an answer', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswerRepository: IAnswersRepository = {
+    create: async (answer : Answer) => {
+        return;
+    }
+}
 
-    const answer = answerQuestion.execute({
+test('Create an answer', async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+
+    const answer = await  answerQuestion.execute({
         questionId: '1',
         instructorId:  '1',
         content: 'Nova resposta'
