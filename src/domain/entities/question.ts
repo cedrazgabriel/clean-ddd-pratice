cb843829cc80f36bc8f19d8b3b89afdf1923f222
Gabriel Cedraz <cedrazdev@gmail.com>
2024-03-05 19:42:22 -0300

    feat: Refactor entity classes to extend EntityBase

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,23 +1,10 @@
-import { randomUUID } from "node:crypto";
 import { Slug } from "./value-objects/slug";
+import { EntityBase } from "../../core/entities/entity";
 interface QuestionProps {
     title: string
     content: string
     authorId: string
     slug: Slug 
 }
-export class Question {
-    public id: string;
-    public title: string;
-    public slug: Slug;
-    public content: string;
-    public authorId: string;
-
-    constructor(props: QuestionProps, id?: string){
-        this.title = props.title
-        this.content = props.content
-        this.slug = props.slug
-        this.authorId = props.authorId
-        this.id = id ?? randomUUID()
-    }
+export class Question extends EntityBase<QuestionProps>{
 }
