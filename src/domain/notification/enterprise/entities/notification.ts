8feb59c2f08739062bf61ba2e28b7de52f8ce6e7
Gabriel Cedraz <cedrazdev@gmail.com>
2024-04-13 14:30:06 -0300

    feaat: create notification entity

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,47 @@
+import { EntityBase } from 'src/core/entities/entity'
+import { UniqueEntityId } from 'src/core/entities/unique-entity-id'
+import { Optional } from 'src/core/types/optional'
+
+interface NotificationProps {
+  recipientId: UniqueEntityId
+  title: string
+  content: string
+  readAt?: Date
+  createdAt: Date
+}
+export class Notification extends EntityBase<NotificationProps> {
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get readAt() {
+    return this.props.readAt
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  static create(
+    props: Optional<NotificationProps, 'createdAt'>,
+    id?: UniqueEntityId,
+  ) {
+    const notification = new Notification(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return notification
+  }
+}
